2f8449f76d67d0126aa82cc42d6a4054fd65a161
Bruno Ventura Gross <bvgross@gmx.com>
2025-06-04 22:09:25 -0300

    primeira aula

---
+++ aula01/meu-app-aula01/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const series = ["Ted Lasso", "Dexter", "Gilmore Girls", "Severance", "Stranger Things"]
+  const numeros = [1, 2, 3, 4, 5, 6, 7]
+
+  return (
+    <>
+      {series.map((item, index) => (
+        <h1 key={index}>{item}</h1>
+      ))}
+      <h1>Teste</h1>
+
+      {numeros
+        .filter((numero) => numero > 3)
+        .map((numeros, index) => (
+          <p key={index}>{numeros}</p>
+        ))}
+
+    </>
+  )
+}
+export default App
